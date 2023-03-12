3c902a8bdf3fd79410366e6461a4108e02cf7fdf
T-Moody <69010221+T-Moody@users.noreply.github.com>
2023-03-12 19:32:10 -0400

    Delete extra code

--- Simplified-Chinese-HSK-3.0-SRS-Heatmap/Simplified-Chinese-HSK-3.0-SRS-Heatmap/Data/AnkiDbContext.cs
+++ Simplified-Chinese-HSK-3.0-SRS-Heatmap/Simplified-Chinese-HSK-3.0-SRS-Heatmap/Data/AnkiDbContext.cs
@@ -40,10 +40,6 @@ public partial class AnkiDbContext : DbContext
 
     public virtual DbSet<Template> Templates { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Datasource=Y:\\programProjects\\collection.anki2");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Card>(entity =>
