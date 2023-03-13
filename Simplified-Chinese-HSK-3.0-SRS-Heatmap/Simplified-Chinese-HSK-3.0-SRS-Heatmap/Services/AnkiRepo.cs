596b8ad2c80d1d7dfabb1808ce85e17cfb9a295d
T-Moody <69010221+T-Moody@users.noreply.github.com>
2023-03-13 17:11:53 -0400

    Refractor code for repos.

---
+++ Simplified-Chinese-HSK-3.0-SRS-Heatmap/Simplified-Chinese-HSK-3.0-SRS-Heatmap/Services/AnkiRepo.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Simplified_Chinese_HSK_3._0_SRS_Heatmap.Data;
+using Simplified_Chinese_HSK_3._0_SRS_Heatmap.infrastructure;
+using Simplified_Chinese_HSK_3._0_SRS_Heatmap.Models;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Simplified_Chinese_HSK_3._0_SRS_Heatmap.Services
+{
+    public class AnkiRepo : IAnki
+    {
+        public AnkiDbContext _ankiContext;
+
+        /// <summary>
+        /// Read in files and create context.
+        /// </summary>
+        public AnkiRepo(AnkiDbContext ankiContext)
+        {
+            _ankiContext = ankiContext;
+        }
+ 
+        /// <summary>
+        /// Get list of characters and days from file and map each character to day.
+        /// </summary>
+        /// <returns>Dictionary of characters and number of days.</returns>
+        public Dictionary<string, int> GetDictionary()
+        {
+            string removalPattern = @"\s*\[.*?\]\s*";
+            string replacement = "";
+
+            Dictionary<string, int> hskDictionary = _ankiContext.Cards
+                .Join(_ankiContext.Notes, card => card.Nid, note => note.Id, (card, note) => new { Sfld = note.Sfld, card.Ivl })
+                .AsEnumerable()
+                .GroupBy(x => Regex.Replace(x.Sfld, removalPattern, replacement))
+                .ToDictionary(g => g.Key, g => g.First().Ivl);
+
+            return hskDictionary;
+        }
+
+        
+
+        /// <summary>
+        /// Get max days after joining chards and notes.
+        /// </summary>
+        /// <returns>Integer max number of days.</returns>
+        public int GetMaxDays()
+        {
+            return _ankiContext.Cards.Join(_ankiContext.Notes, card => card.Nid, note => note.Id, (card, note) => new { Sfld = note.Sfld, card.Ivl })
+                   .Max(x => x.Ivl);
+        }
+    }
+}
--- Simplified-Chinese-HSK-3.0-SRS-Heatmap/Simplified-Chinese-HSK-3.0-SRS-Heatmap/Services/HskRepo.cs
+++ Simplified-Chinese-HSK-3.0-SRS-Heatmap/Simplified-Chinese-HSK-3.0-SRS-Heatmap/Services/HskRepo.cs
@@ -1,29 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Simplified_Chinese_HSK_3._0_SRS_Heatmap.Data;
-using Simplified_Chinese_HSK_3._0_SRS_Heatmap.infrastructure;
+﻿using Simplified_Chinese_HSK_3._0_SRS_Heatmap.infrastructure;
 using Simplified_Chinese_HSK_3._0_SRS_Heatmap.Models;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Simplified_Chinese_HSK_3._0_SRS_Heatmap.Services
 {
     public class HskRepo : IHsk
     {
         public readonly string _hskAllFileName = @"wwwroot\files\all_HSK.txt"; // Name of the file constaining hsk values. Each line must use \t as a delimeter. EX of a line: 的 Characters  HSK1
-        public readonly string _hskDictionaryFileName = @"wwwroot\files\dictionary_HSK.txt"; // File with characters and days. Delimeter is \t. EX: 的[de5;结构助词的] 
-        public AnkiDbContext _ankiContext;
-
         private string[] _hskAllContext;
-        private string[] _hskDictionaryContext;
 
         /// <summary>
-        /// Read in files and create context.
+        /// Constructor reads lines from file into string array.
         /// </summary>
-        public HskRepo(AnkiDbContext ankiContext)
+        public HskRepo() 
         {
             _hskAllContext = File.ReadAllLines(_hskAllFileName);
-            _hskDictionaryContext = File.ReadAllLines(_hskDictionaryFileName);
-            _ankiContext = ankiContext;
         }
 
         /// <summary>
@@ -40,24 +30,6 @@ namespace Simplified_Chinese_HSK_3._0_SRS_Heatmap.Services
         }
 
         /// <summary>
-        /// Get list of characters and days from file and map each character to day.
-        /// </summary>
-        /// <returns></returns>
-        public Dictionary<string, int> GetDictionary()
-        {
-            string removalPattern = @"\s*\[.*?\]\s*";
-            string replacement = "";
-
-            Dictionary<string, int> hskDictionary = _ankiContext.Cards
-                .Join(_ankiContext.Notes, card => card.Nid, note => note.Id, (card, note) => new { Sfld = note.Sfld, card.Ivl })
-                .AsEnumerable()
-                .GroupBy(x => Regex.Replace(x.Sfld, removalPattern, replacement))
-                .ToDictionary(g => g.Key, g => g.First().Ivl);
-
-            return hskDictionary;
-        }
-
-        /// <summary>
         /// Convert a hskLine string to HskModel.
         /// </summary>
         /// <param name="hskLine">Ex: 的 Characters  HSK1</param>
@@ -71,15 +43,6 @@ namespace Simplified_Chinese_HSK_3._0_SRS_Heatmap.Services
             string level = currentLine[2];
 
             return new HskModel(character, type, level);
-        }
-
-        /// <summary>
-        /// Get max days from dictionary string array.
-        /// </summary>
-        /// <returns>Integer max number of days.</returns>
-        public int GetMaxDays()
-        {
-            return _hskDictionaryContext.Select(line => int.Parse(line.Split('\t')[1])).Max();
         }
     }
 }
