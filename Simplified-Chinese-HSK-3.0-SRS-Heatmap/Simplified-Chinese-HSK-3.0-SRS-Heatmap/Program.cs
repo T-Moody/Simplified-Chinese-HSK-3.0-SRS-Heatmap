495d0b0a5bd05a1de9ab46a52154cff51de03156
T-Moody <69010221+T-Moody@users.noreply.github.com>
2023-03-14 12:42:40 -0400

    Add linux and mac logic to get database string.

--- Simplified-Chinese-HSK-3.0-SRS-Heatmap/Simplified-Chinese-HSK-3.0-SRS-Heatmap/Program.cs
+++ Simplified-Chinese-HSK-3.0-SRS-Heatmap/Simplified-Chinese-HSK-3.0-SRS-Heatmap/Program.cs
@@ -2,12 +2,13 @@ using Microsoft.EntityFrameworkCore;
 using Simplified_Chinese_HSK_3._0_SRS_Heatmap.Data;
 using Simplified_Chinese_HSK_3._0_SRS_Heatmap.infrastructure;
 using Simplified_Chinese_HSK_3._0_SRS_Heatmap.Services;
+using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Get database connection.
 builder.Services.AddDbContext<AnkiDbContext>(options =>
-   options.UseSqlite(GetDatabaseFile()));
+   options.UseSqlite(DbConnectionHelper.GetDatabaseConnectionString()));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -38,36 +39,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
-// Dynamically finds the anki database file.
-static string GetDatabaseFile()
-{
-    string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-    string anki2Folder = Path.Combine(appDataFolder, "Anki2");
-    string[] userFolders = Directory.GetDirectories(anki2Folder, "*", SearchOption.TopDirectoryOnly);
-
-    if (userFolders.Length == 0)
-    {
-        Console.WriteLine("Could not find any Anki user folders.");
-        return "";
-    }
-
-
-    string collectionPath = "";
-    int counter = 0;
-    bool found = false;
-
-    while (!found)
-    {
-        collectionPath = Path.Combine(userFolders[counter], "collection.anki2");
-
-        if (File.Exists(collectionPath))
-        {
-            found = true;
-        }
-
-        counter++;
-    }
-
-    return "Filename=" + collectionPath;
-}
